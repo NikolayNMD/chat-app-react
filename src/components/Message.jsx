81bb134170b07ffff144d45aac9aeb9ff03c6ab1
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2025-04-27 03:59:31 +0300

    add persist for local save history and create user and bot spans with some styles feachures

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Message = ({ sender, text }) => {
   return (
     <div className={`message ${sender}`}>
+      <span className="sender">{sender === "user" ? "Користувач" : "Бот"}</span>
       <div className="bubble">{text}</div>
     </div>
   );
--- src/store/chatStore.js
+++ src/store/chatStore.js
@@ -1,13 +1,31 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useChatStore = create((set) => ({
-  messages: [],
-  addUserMessage: (text) =>
-    set((state) => ({
-      messages: [...state.messages, { id: Date.now(), text, sender: "user" }],
-    })),
-  addBotMessage: (text) =>
-    set((state) => ({
-      messages: [...state.messages, { id: Date.now(), text, sender: "bot" }],
-    })),
-}));
+export const useChatStore = create(
+  persist(
+    (set) => ({
+      messages: [],
+      addUserMessage: (text) =>
+        set((state) => ({
+          messages: [
+            ...state.messages,
+            { id: Date.now(), text, sender: "user" },
+          ],
+        })),
+      addBotMessage: (text) =>
+        set((state) => ({
+          messages: [
+            ...state.messages,
+            { id: Date.now(), text, sender: "bot" },
+          ],
+        })),
+      clearMessages: () =>
+        set(() => ({
+          messages: [],
+        })),
+    }),
+    {
+      name: "chat-messages",
+    }
+  )
+);
